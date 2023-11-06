2f41d7c4873402af1ba3918e6997ec64c5e2abd6
pluveto <i@pluvet.com>
2023-11-06 15:20:22 +0800

    feat: check

--- main.c
+++ main.c
@@ -1,3 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+struct CheckResult {
+  int ok;
+  char emsg[100];
+};
+
+struct CheckResult is_all_value(const void* mem, size_t size, char value) {
+  struct CheckResult result = {1, ""};
+  const char* p = mem;
+  for (size_t i = 0; i < size; i++) {
+    if (p[i] != value) {
+      result.ok = 0;
+      snprintf(result.emsg, sizeof(result.emsg), "mem[%zu] = %d", i, p[i]);
+      break;
+    }
+  }
+  return result;
+}
+
 int main() {
+  char mem[100];
+  struct CheckResult result = is_all_value(mem, sizeof(mem), 0);
+  if (!result.ok) {
+    printf("NG: %s\n", result.emsg);
+    return 1;
+  }
+
+  memset(mem, 1, sizeof(mem));
+  result = is_all_value(mem, sizeof(mem), 1);
+  if (!result.ok) {
+    printf("NG: %s\n", result.emsg);
+    return 1;
+  }
   return 0;
 }
